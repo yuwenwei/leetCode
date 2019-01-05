fc9b2351a74feb62577aad22e3d2570db90648e4
yuwenwei <635501270@qq.com>
2019-01-05 13:59:48 +0800

    解决新冲突

--- leetCode/src/leetCode/FindNumber.java
+++ leetCode/src/leetCode/FindNumber.java
@@ -4,8 +4,9 @@ public class FindNumber {
 	
 	public static void main(String[] args) {
 		int b = 3;
+		int a = 4;
 		//冲突处理
-		System.out.println();
-		String a = "";
+		System.out.println(a+b);
+		String s = "";
 	}
 }
