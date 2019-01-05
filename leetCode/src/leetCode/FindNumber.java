bacd7794f5d118bc9d75d44bf5852450761156ee
yuwenwei <635501270@qq.com>
2019-01-05 13:57:18 +0800

    Update FindNumber.java

--- leetCode/src/leetCode/FindNumber.java
+++ leetCode/src/leetCode/FindNumber.java
@@ -3,9 +3,10 @@ package leetCode;
 public class FindNumber {
 	
 	public static void main(String[] args) {
+		int a = 4;
 		int b = 3;
 		//冲突处理
 		System.out.println();
-		String a = "";
+		String str = "";
 	}
 }
